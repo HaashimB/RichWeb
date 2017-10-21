9c720b2be3be6e2a2418b677b5f99ef980fcf373
hashdoge <hashdoggie@gmail.com>
2017-10-21 15:36:58 +0100

    Started Lab 3  - Copied over Calculator directory to start part 1  - Set up NodeJs, NPM, RxJs, and Webpack

---
+++ RichWeb/Lab 3/Calculator/Calculator.js
@@ -0,0 +1,19 @@
+
+
+//document.getElementsByTag("button").onclick='changeText()';
+
+function changeText(x){
+
+    var elem = document.getElementById("inputField");
+    if(x === 'C'){
+        document.getElementById('inputField').value = null;
+
+    }else if(x === '=') {
+        document.getElementById('inputField').value = eval(elem.value);
+    }else{
+        elem.value += x;
+
+    }
+
+
+}
