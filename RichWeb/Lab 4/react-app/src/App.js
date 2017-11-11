4bba25df8ecfeee11a678ace304987c536c781f0
Hashdoge <hashdoggie@gmail.com>
2017-11-11 17:59:14 +0000

    Made changes to render()

--- RichWeb/Lab 4/react-app/src/App.js
+++ RichWeb/Lab 4/react-app/src/App.js
@@ -3,10 +3,52 @@ import React from 'react';
 import axios from 'axios';
 
 
-const App = () => <h1>Hello</h1>
 
-ReactDOM.render(
 
-    <App />,
-    document.getElementById('root')
-);
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            query: props.query
+        };
+    }
+    componentWillUpdate(){
+        axios({
+            method:'get',
+            baseURL:'https://swapi.co/api/',
+            url: 'people/?search=' + this.state.query
+        })
+            .then(response => {
+                let data = response.data.results.map(obj => obj);
+                console.log(data);
+                this.setState({data});
+
+            });
+    }
+
+    update(e){
+        this.setState({query: e.target.value})
+    }
+    render(){
+        // return React.createElement('h1',null,'EGG BOY')
+        //console.log(data);
+
+
+        return(
+            <div>
+                <input id = "query"  type = "text" placeholder = "Luke Skywalker"/>
+                <button onClick ={ this.update.bind(this) }>Search</button>
+                <h1>{this.state.query}</h1>
+                <ul>
+                    {this.state.data.map(post =>
+                        <li>{post.name}</li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+export default App
