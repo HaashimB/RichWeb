c6669701d5c4b4ef852495100f740a03bddb0f16
hashdoge <hashdoggie@gmail.com>
2017-11-11 13:52:29 +0000

    Set Up REACT JSX Got rid of unnecessary files

---
+++ RichWeb/Lab 4/react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Rx from 'rxjs/Rx';
+import axios from 'axios';
+
+
+const App = () => <h1>Hello</h1>
+
+ReactDOM.render(
+
+    <App />,
+    document.getElementById('root')
+);
